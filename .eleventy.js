be64bfdc631451c62152cd5f8505b6dc32956f4c
Mat Marquis <mat@matmarquis.com>
2018-08-27 10:20:09 -0400

    Imagine, if you will—if you _dare_—a website.

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+  //eleventyConfig.addPassthroughCopy("_src/_assets/img");
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html"
+    ],
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "_src",
+      includes: "_layouts",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
---
+++ _src/index.md
@@ -0,0 +1,7 @@
+---
+layout: partials/home.njk
+---
+
+## Okay, initial commit.
+
+I wrote a book; who wants to buy a copy?
